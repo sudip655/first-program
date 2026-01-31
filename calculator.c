e3ca7badbb2d649d30024c052f9d3c3dbb17fe70
sudip655 <sudippandey655@gmail.com>
2026-01-31 15:17:50 +0545

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main()
+{
+    float num1, num2, result;
+    char op;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    switch(op)
+    {
+        case '+':
+            result = num1 + num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '/':
+            if(num2 != 0)
+            {
+                result = num1 / num2;
+                printf("Result = %.2f\n", result);
+            }
+            else
+            {
+                printf("Division by zero is not allowed\n");
+            }
+            break;
+
+        default:
+            printf("Enter a valid operator\n");
+    }
+
+    return 0;
+}
